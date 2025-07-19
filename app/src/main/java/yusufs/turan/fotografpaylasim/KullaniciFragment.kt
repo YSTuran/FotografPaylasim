e512e45d436baf9e62e343c253dba0932b2ffe1e
Yusuf S. Turan <siryusufsus@gmail.com>
2025-07-19 12:25:24 +0300

    Kullanıcı kayıt ve giriş ekranı yapıldı

---
+++ app/src/main/java/yusufs/turan/fotografpaylasim/KullaniciFragment.kt
@@ -0,0 +1,48 @@
+package yusufs.turan.fotografpaylasim
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import yusufs.turan.fotografpaylasim.databinding.FragmentKullaniciBinding
+
+
+class KullaniciFragment : Fragment() {
+    private var _binding:FragmentKullaniciBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentKullaniciBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonKayit.setOnClickListener { kaydol(it) }
+        binding.buttonGiris.setOnClickListener { giris(it) }
+    }
+
+    fun kaydol(view: View){
+        println("Kayıt Yapıldı")
+    }
+
+    fun giris(view: View){
+        println("Giriş Yapıldı")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
