792945c9f890bd999f8822f0a84493ff8b86e4c9
Yusuf S. Turan <siryusufsus@gmail.com>
2025-07-19 16:52:06 +0300

    Gönderi yükleme sayfası oluşturulmaya başlandı

--- app/src/main/java/yusufs/turan/fotografpaylasim/YuklemeFragment.kt
+++ app/src/main/java/yusufs/turan/fotografpaylasim/YuklemeFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import yusufs.turan.fotografpaylasim.databinding.FragmentKullaniciBinding
+import yusufs.turan.fotografpaylasim.databinding.FragmentYuklemeBinding
 
 class YuklemeFragment : Fragment() {
 
+    private var _binding: FragmentYuklemeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -19,7 +22,27 @@ class YuklemeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_yukleme, container, false)
+        _binding = FragmentYuklemeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    fun gonderiYukle(view: View){
+        binding.buttonYukle.setOnClickListener { gonderiYukle(it) }
+        binding.imageView.setOnClickListener{gorselSec(it)}
+    }
+
+    fun gorselSec(view: View){
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
